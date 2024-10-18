3ef4b55ab7d18e7b37a3814adc1bd03c7649cd5b
turleyduque <turleyd@bu.edu>
2024-10-18 14:18:04 -0400

    education labels

--- Clean_data.R
+++ Clean_data.R
@@ -1,4 +1,15 @@
 
 library(tidyverse)
 library(haven)
-data2 <- read_xpt("LLCP2023.XPT")
+data2 <- read_xpt("dataset-ignore/LLCP2023.XPT")
+
+data2 <- data2 |>
+  mutate(EDUCA = case_when (
+    EDUCA == 1 ~ "None/Kindergarten",
+    EDUCA == 2 ~ "Elementary",
+    EDUCA == 3 ~ "Some High School",
+    EDUCA == 4 ~ "High School Graduate",
+    EDUCA == 5 ~ "Some College",
+    EDUCA == 6 ~ "College Graduate",
+    EDUCA == 9 ~ "Refused",
+  ))
