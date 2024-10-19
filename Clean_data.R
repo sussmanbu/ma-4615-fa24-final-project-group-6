d22888f6317d9bed4109fe51b084978b5c66e232
Qizheng Yang <qizhengy@bu.edu>
2024-10-18 21:29:22 -0400

    It is yankee

--- Clean_data.R
+++ Clean_data.R
@@ -1,3 +1,28 @@
 library(tidyverse)
 library(haven)
+
 data2 <- read_xpt("dataset-ignore/LLCP2023.XPT")
+
+colnames(data2)
+
+if ("GENHLTH" %in% colnames(data2)) {
+  
+  colnames(data2)[colnames(data2) == "GENHLTH"] <- "Health Status"
+  
+  data2$`Health Status` <- as.factor(data2$`Health Status`)
+  
+  data2$`Health Status` <- recode(data2$`Health Status`, 
+                                  `1` = "Excellent",
+                                  `2` = "Very Good",
+                                  `3` = "Good",
+                                  `4` = "Fair",
+                                  `5` = "Poor",
+                                  `7` = "Don’t Know/Not Sure",
+                                  `9` = "Refused")
+  
+  head(data2)
+  
+} else {
+  print("GENHLTH column not found in the dataset.")
+}
+
