884b82dd992c641933dc1c04ad0beaeb8407d88e
Cindy Frempong <cinfrem@bu.edu>
2024-11-13 21:04:28 -0500

    cin com

--- Clean_data.R
+++ Clean_data.R
@@ -298,7 +298,7 @@ interaction <- aov(PHYSHLTH ~ Health_status * stress_feeling_frequency + emotion
 summary(interaction)
 
 
-library(ggplot2)
+library(ggplot2) 
 
 Phys_Health <- ggplot(anova_data, aes(x = Health_status, y = PHYSHLTH, fill = Health_status)) + 
   theme_bw() +  
