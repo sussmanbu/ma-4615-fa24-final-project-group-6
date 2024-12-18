6c02e68f922eb6fc654d2d6a730211927d039840
turleyduque <turleyd@bu.edu>
2024-12-18 12:39:05 -0500

    shiny

--- Clean_data.R
+++ Clean_data.R
@@ -650,5 +650,6 @@ shiny_table <- US_map_filtered |>
   left_join(variables_table, by = 'State')
 
 
-saveRDS(shiny_table, file = "shinydataset/shiny_table.rds")
+saveRDS(shiny_table, file = "shinydataset/shiny_table_updated.rds")
+saveRDS(shiny_table, file = "scripts/shiny_table.rds")
 
